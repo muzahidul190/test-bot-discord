0ffb49dea244acb04679e6bb7221381738dcf638
muzahidul190 <muzahidul190@gmail.com>
2018-03-09 21:38:25 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,111 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client({disableEveryone: true});
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
+let xp = require("./xp.json");
+let purple = botconfig.purple;
 
-bot.on('ready', async () => {
-    console.log('I am ready!');
-    bot.user.setActivity("Getting Constructed", {type: "WATCHING"});
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-bot.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm"){
-        return message.reply('Don\'t DM me now. I\'m still a kid :cold_sweat:');
-    }
-    
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    
-    if (cmd === 'frn') {
-        return message.channel.sendMessage('I am alive.').then(message => message.delete(5000));  	
-    }
-    if(cmd == 'qwerty'){
-        return message.reply('Are you on your keyboard?');
-    }
-    if(cmd === 'clr'){
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('Sorry, you do not have permission.');
-        message.channel.bulkDelete(3).then(() => {
-            message.channel.sendMessage('3 messages deleted.').then(message => message.delete(5000));
-        });
-    }
-    if(cmd == 'invite'){
-        return message.channel.send('Invitation link is https://discord.gg/crtrH5y');
-    }
-    if(cmd == 'help'){
-        return message.channel.send('<@325510053970837505> is still constructing me. ``Help`` menu will be added **soon**.');
-    }
+bot.on("ready", async () => {
+
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
 });
-module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('Oops, you dont have permission.');
-    if(!args[0]) return message.channel.send('Oops!');
-    message.channel.bulkDelete(args[0]);
-}
-module.exports.help = {
-    name: "clear"
-}
+
+
+bot.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let coinAmt = Math.floor(Math.random() * 15) + 1;
+  let baseAmt = Math.floor(Math.random() * 15) + 1;
+  console.log(`${coinAmt} ; ${baseAmt}`);
+
+  if(coinAmt === baseAmt){
+    coins[message.author.id] = {
+      coins: coins[message.author.id].coins + coinAmt
+    };
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if (err) console.log(err)
+  });
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#0000FF")
+  .addField("ðŸ’¸", `${coinAmt} coins added!`);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  }
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+
+  let prefix = prefixes[message.guild.id].prefixes;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+});
 
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
