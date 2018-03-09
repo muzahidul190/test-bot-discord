3580244140443c813d95c170b1e8cb61bbaf8fab
muzahidul190 <muzahidul190@gmail.com>
2018-03-10 01:05:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ bot.on("ready", async () => {
   bot.user.setGame("WoW!! It's working!!!", {type: "CHILLING"});
 });
 
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return message.channel.send("Hey, What\'s up? I\'m still under construction. Don\'t DM me now.");
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}bott`){
+    return message.channel.send("Hello!");
+  }
+  if(cmd == 'invitelink'){
+        return message.channel.send('Invitation link is https://discord.gg/crtrH5y');
+}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
