19a1facec0ec77b722610702b2169c2abea3651c
muzahidul190 <muzahidul190@gmail.com>
2018-03-10 04:37:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
   //bot.user.setGame("Getting Coded on GitHub");
-  bot.user.setActivity('Codes on GitHub', { type: 'WATCHING' })
+  bot.user.setActivity('Coded on GitHub', { type: 'Getting' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
   .catch(console.error);
 });
