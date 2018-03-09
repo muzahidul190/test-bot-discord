9d3b89d0ec6d8e90e6a5f585f217ee3e99cf19ba
muzahidul190 <muzahidul190@gmail.com>
2018-03-10 00:29:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
-  bot.user.setGame("Getting Constructed by HF!");
+  bot.user.setGame("WoW!! It's working!!!");
 });
 
 
