75502bcf6cbdb71c5b3ccff9316e2e43d1bf1379
muzahidul190 <muzahidul190@gmail.com>
2018-03-12 04:57:40 +0600

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!clear 15
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, pal. You don\'t have permission to delete messeges.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, pal. You don\'t have permission to delete messeges.").then(msg => msg.delete(5000));
   if(!args[0]) return message.channel.send("Please enter ammount of messages to delete. Example \`\`cl 5\`\`").then(msg => msg.delete(5000));
   var num = eval(args[0] + "+" + 1);
   message.channel.bulkDelete(num).then(() => {
