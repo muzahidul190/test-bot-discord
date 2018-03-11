15a1bc43e17fd8227f3ed4ee84e2099efe9016db
muzahidul190 <muzahidul190@gmail.com>
2018-03-12 00:36:58 +0600

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!rUser) return message.channel.send("Can't find the user!");
+  if(!rUser) return;
   let reason = args.join(" ").slice(22);
   if(!reason) return message.channel.send("Please enter reason... \n**Example:** \`\`report <mention user> <Reason>\`\`")
 
