8f4b96a4378b21cc3f3b2b359c34911fafca0f2f
muzahidul190 <muzahidul190@gmail.com>
2018-03-12 04:55:26 +0600

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  //!clear 15
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, pal. You don\'t have permission to delete messeges.");
+  if(!args[0]) return message.channel.send("Please enter ammount of messages to delete. Example \`\`cl 5\`\`").then(msg => msg.delete(5000));
+  var num = eval(args[0] + "+" + 1);
+  message.channel.bulkDelete(num).then(() => {
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
+}
+
+module.exports.help = {
+  name: "clear"
+}
