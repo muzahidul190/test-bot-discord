6de4e0fa8c634c4873972bc50426b6970a04ae60
muzahidul190 <muzahidul190@gmail.com>
2018-03-12 14:46:05 +0600

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
   //!clear 15
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, pal. You don\'t have permission to delete messeges.").then(msg => msg.delete(5000));
-  if(!args[0]) return message.channel.send("Please enter ammount of messages to delete. Example \`\`cl 5\`\`").then(msg => msg.delete(5000));
+  if(!args[0]) return message.channel.send("Please enter ammount of messages to delete. Example \`\`clear 5\`\`").then(msg => msg.delete(5000));
   var num = eval(args[0] + "+" + 1);
   message.channel.bulkDelete(num).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
