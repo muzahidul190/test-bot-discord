9c705a5bf3ac76e8900bff57ec7db145c880598a
muzahidul190 <muzahidul190@gmail.com>
2018-03-27 11:55:06 +0600

    Update bankresister.js

--- commands/bankresister.js
+++ commands/bankresister.js
@@ -3,21 +3,26 @@ const fs = require("fs");
 let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.channel.id !== `427944880170336289`) return message.channel.send(`\`\`bankregister\`\` command works only in <#427944880170336289> channel.`);
-
-  if(!coins[message.author.id]){
-    return message.channel.send(`Congratulations <@${message.author.id}>, your account is opened Successfully!`).then(() => {
-      coins[message.author.id] = {
-        coins: 5000,
-        time: Date.now()
-      }
-      fs.writeFile("./coins.json", JSON.stringify(coins, null, 2), (err) => {
-        if(err) cosole.log(err)
+  if(!args[0]){
+    if(message.channel.id !== `427944880170336289`) return message.channel.send(`\`\`bankregister\`\` command works only in <#427944880170336289> channel.`);
+ 
+    if(!coins[message.author.id]){
+      let tim = Date.now() - 28800000;
+      return message.channel.send(`Congratulations <@${message.author.id}>, your account is opened Successfully!`).then(() => {
+        coins[message.author.id] = {
+         coins: 5000,
+         time: tim
+        }
+        fs.writeFile("./coins.json", JSON.stringify(coins, null, 2), (err) => {
+         if(err) cosole.log(err)
+       });
       });
-    });
-  }else{
-    return message.channel.send(`${message.author.username}, you are already registered!`)
-  }
+     }else{
+      return message.channel.send(`${message.author.username}, you are already registered!`)
+     }
+    }else{
+      return;
+     }
 
 }
 
