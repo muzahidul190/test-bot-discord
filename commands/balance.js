bca235d68bdebe7bc529be52d793d69edc129f34
muzahidul190 <muzahidul190@gmail.com>
2018-03-30 15:48:27 +0600

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -2,30 +2,24 @@ const Discord = require("discord.js");
 let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.channel.id !== `427944880170336289`) return message.channel.send(`\`\`bal\`\` command works only in <#427944880170336289> channel.`);
-  let cUser = message.mentions.users.first() || message.author;
-  let dUser = message.author.id;
-  if(!cUser){
-    cUser = dUser;
-  }else{
-    cUser = cUser;
-  }
-  //!coins
-  if(!coins[cUser.id]){
-    let cUser = message.mentions.users.first() || message.author;
-    return message.channel.send(`Sorry ${cUser.username} doesn't have an account yet!`)
-  }
+  let target = message.mentions.users.first() || message.author;
 
-  let uCoins = coins[cUser.id].coins;
-  let tst = coins[cUser.id].time;
+  con.query(`SELECT * FROM users WHERE sid = '${target.id}'`, (err, rows) => {
+    if(err) throw err;
 
-  if(uCoins === null) uCoins = 0;
+    if(!rows[0]) return message.channel.send(`${target.username} doesn\'t have a bank account!`);
+
+    let xp = rows[0].credit;
+
+    let coinEmbed = new Discord.RichEmbed()
+    .setAuthor(target.username)
+    .setColor("#00FF00")
+    .addField("💰", xp)
+    .addField("Testing", `Hello`);
 
 
-
-
-  message.channel.send(`${cUser.username}\'s balance is **$${uCoins}**`);
-
+    message.channel.send(coinEmbed);
+  });
 }
 
 module.exports.help = {
