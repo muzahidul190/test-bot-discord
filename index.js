0ea925846b9e36d653ba874c09f242e3a5b64350
muzahidul190 <muzahidul190@gmail.com>
2018-03-31 11:56:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ fs.readdir("./commands/", (err, files) => {
     bot.commands.set(props.help.name, props);
   });
 });
-/*var con = mysql.createConnection({
-  host: sql10.freesqldatabase.com, // process.env.DB_HOST,//sql10.freesqldatabase.com
-  user: sql10229343, //process.env.DB_USER,//id5207968_muzahidul190--sql10229343
-  password: 7K336cULGd, //process.env.DB_PASS,//sawakSAHAK726251--7K336cULGd
-  database: sql10229343 //process.env.DB_NAME//id5207968_discordbot--sql10229343
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 con.connect(err => {
   if(err) throw err;
   console.log("Connected to the database!");
-});*/
+});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
   bot.user.setActivity('Codes on GitHub', { type: 'WATCHING' })
