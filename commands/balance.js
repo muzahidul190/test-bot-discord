8d7397ce19ab79a83e25a9b27c0b414a945f25e1
muzahidul190 <muzahidul190@gmail.com>
2018-04-01 08:18:13 +0600

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -2,23 +2,10 @@ const Discord = require("discord.js");
 let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args, con) => {
-  let target = message.mentions.users.first() || message.author;
-
-  con.query(`SELECT * FROM users WHERE sid = '${target.id}'`, (err, rows) => {
-    if(err) throw err;
-
-    if(!rows[0]) return message.channel.send(`${target.username} doesn\'t have a bank account!`);
-
-    let xp = rows[0].credit;
-
-    let coinEmbed = new Discord.RichEmbed()
-    .setAuthor(target.username)
-    .setColor("#00FF00")
-    .addField("💰", xp)
-    .addField("Testing", `Hello`);
 
 
-    message.channel.send(coinEmbed);
+
+    message.channel.send(`This command is under maintenance!!! `);
   });
 }
 
